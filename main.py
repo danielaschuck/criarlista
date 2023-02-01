7b83e8b980664fef4eb3e34e5ce29dc367c9b572
Daniela Schuck <114201069+danielaschuck@users.noreply.github.com>
2023-02-01 20:27:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+num_elements=(int(input("Quantos elementos você deseja adicionar na lista? ")))
+
+
+lista = []
+
+for i in range(num_elements):
+   element =(input("Insira o elemento {}: "))
+   lista.append(element)
+
+print("Sua lista é:", lista)
+
+
+
